763f5899d46aec0ce511479c88f88fdfab345cb6
Viktoria Silver <viktoria.sudraba@gmail.com>
2024-05-11 22:00:44 +0100

    Improve char and &str comparison in Lesson 4

--- 04_chars_bools_unit_types/char.rs
+++ 04_chars_bools_unit_types/char.rs
@@ -6,7 +6,8 @@ fn main() {
     assert_eq!(size_of_val(&c1), 4);
 
     let mut s: &str = "a"; // str will take as much space as necessary
-    println!("{}", s.len()); // "a" will be 1 byte
+    println!("{}", s.len()); // "a" will be 1 byte of length
+    println!("{}", size_of_val(&s)); // the size of the variable s will be 16 bytes, because it is a string
 
     let c2: char = '牛'; // chars are written with single quotes
     println!("{}", size_of_val(&c2)); // will also output 4 because it is also a char
@@ -14,8 +15,15 @@ fn main() {
 
     s = "牛"; // strings are written with double quotes
     println!("{}", s.len()); // this will be 3 bytes because it is a chinese character as a string
+    println!("{}", size_of_val(&s)); // the size of the variable s will be 16 bytes, regardless of what is the value or lenght of it
 
-    // reserved space is not the same as length
+
+    // chars are stored directly into the stack
+    // strings are stored in the heap and the stack holds a string structure:
+        // 1) pointer to where the string starts in the heap
+        // 2) length of the string
+        // the above are both 8 bytes of data, hence 2 * 8 = 16 bytes to store a string
+
 
     let c3 = '牛'; // needs to have single quotes because the function expects a char, not a string
     print_char(c3);
